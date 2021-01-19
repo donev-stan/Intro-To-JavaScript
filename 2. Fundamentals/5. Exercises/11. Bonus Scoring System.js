10baf18b0603bb120a22494f62741f4800603fde
donev-stan <77002111+donev-stan@users.noreply.github.com>
2021-01-19 23:54:44 +0200

    exercises updated (proud I am doing them alone)

---
+++ 2. Fundamentals/5. Exercises/11. Bonus Scoring System.js
@@ -0,0 +1,6 @@
+/*
+    Create a program that calculates bonus points for each student, for a certain course. On the first line, you are going to receive the count of the students for this course. 
+    On the second line, you will receive the count of the lectures in the course. Every course has an additional bonus. You are going to receive it on the third line. 
+    On the next lines, you will be receiving the count of attendances for each student. 
+*/
+
