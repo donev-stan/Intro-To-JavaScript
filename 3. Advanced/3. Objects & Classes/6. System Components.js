4478573622dc74b50d11603b5ead04e7402dfc04
donev-stan <77002111+donev-stan@users.noreply.github.com>
2021-01-31 01:48:14 +0200

    two more for the night

---
+++ 3. Advanced/3. Objects & Classes/6. System Components.js
@@ -0,0 +1,53 @@
+function solve(array){
+    
+    const catalog = {};
+
+    for (const line of array) {
+        const [system, component, sub] = line.split(' | ');
+
+        if (catalog.hasOwnProperty(system) === false) {
+            catalog[system] = {};
+        }
+
+        if (catalog[system].hasOwnProperty(component) === false) {
+            catalog[system][component] = [];
+        }
+
+        catalog[system][component].push(sub);
+    }
+
+    //---   Object.entries(obj)
+    // [
+    //     [key, value]
+    //     [key, value]
+    // ]
+    Object.entries(catalog).sort((a,b) => {
+        return Object.keys(b[1]).length - Object.keys(a[1]).length || a[0].localeCompare(b[0]);
+    }).forEach(([system, component]) => {
+        console.log(system);
+        Object.entries(component)
+        .sort((a, b) => (b[1]).length - (a[1]).length)
+        .forEach(([name, sub]) => {
+            console.log(`|||${name}`);
+            sub.forEach( s =>{
+                console.log(`||||||${s}`);
+            });
+        })
+    });
+}
+
+solve(
+    ['SULS | Main Site | Home Page',
+    'SULS | Main Site | Login Page',
+    'SULS | Main Site | Register Page',
+    'SULS | Judge Site | Login Page',
+    'SULS | Judge Site | Submittion Page',
+    'Lambda | CoreA | A23',
+    'SULS | Digital Site | Login Page',
+    'Lambda | CoreB | B24',
+    'Lambda | CoreA | A24',
+    'Lambda | CoreA | A25',
+    'Lambda | CoreC | C4',
+    'Indice | Session | Default Storage',
+    'Indice | Session | Default Security']
+)
---
+++ 3. Advanced/3. Objects & Classes/8. Tickets.js
@@ -0,0 +1,41 @@
+class Ticket {
+    constructor(descriptor){
+        const [destination, price, status] = descriptor.split('|');
+
+        this.destination = destination;
+        this.price = Number(price);
+        this.status = status;
+    }
+}
+
+function solve(tickets, criteria){
+    
+    const comparator = {
+        destination: (a, b) => a.destination.localeCompare(b.destination),
+        price: (a, b) => a - b,
+        status: (a, b) => a.status.localeCompare(b.status)
+    }
+
+    return tickets.map(t => new Ticket(t)).sort(comparator[criteria]);
+}
+
+let one = solve(
+    ['Philadelphia|94.20|available',
+    'New York City|95.99|available',
+    'New York City|95.99|sold',
+    'Boston|126.20|departed'],
+    'destination'
+)
+
+
+let two = solve(
+    ['Philadelphia|94.20|available',
+    'New York City|95.99|available',
+    'New York City|95.99|sold',
+    'Boston|126.20|departed'],
+    'status'
+)
+
+// Terminal
+console.log(one);
+console.log(two);
