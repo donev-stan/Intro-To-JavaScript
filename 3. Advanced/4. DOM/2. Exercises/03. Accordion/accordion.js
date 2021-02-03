56d3e368463657fe485b34577770a8137a241c43
donev-stan <77002111+donev-stan@users.noreply.github.com>
2021-02-03 23:47:49 +0200

    DOM exercises added

--- 3. Advanced/4. DOM/2. Exercises/03. Accordion/accordion.js
+++ 3. Advanced/4. DOM/2. Exercises/03. Accordion/accordion.js
@@ -1,3 +1,15 @@
 function toggle() {
-   
+  const div = document.querySelector("#extra");
+  const btn = document.querySelectorAll(".button")[0];
+  //const [btn] = Array.from(document.querySelectorAll(".button")); // this returns an array LIKE list. We cast it into an array and then by deconstructing we tak the '0' element
+  //const [btn] = [...document.querySelectorAll(".button")]; -- the same
+
+  console.log(btn.textContent);
+  if (div.style.display === "block") {
+    btn.textContent = "MORE";
+    div.style.display = "none";
+  } else {
+    btn.textContent = "LESS";
+    div.style.display = "block";
+  }
 }
--- 3. Advanced/4. DOM/2. Exercises/04. Order-the-Names/solution.js
+++ 3. Advanced/4. DOM/2. Exercises/04. Order-the-Names/solution.js
@@ -1,4 +1,57 @@
+/*
 function solve() {
+  document.querySelector("button").addEventListener("click", onClick);
+  const list = {};
 
-    //TODO...
+  // съставяме списък
+  //const items = document.querySelectorAll('ol > li');
+  // ('ol > li') - намери всички елементи от тип 'li' които са директни деца на тип 'ol'
+  // a better way:
+  const items = document.querySelector("ol").querySelectorAll('li');
+  items.forEach((el) => {
+    // попълваме асоциативен масив по букви
+    if (el.textContent.trim().length == 0) {
+      return; // like continue
+    }
+    const letter = el.textContent[0].toLocaleUpperCase();
+    list[letter] = el.textContent;
+  });
+
+  function onClick() {
+    const input = document.querySelector("input");
+    const value = input.value;
+    const letter = value[0].toLocaleUpperCase();
+
+    // добавяме към списък
+    if (list.hasOwnProperty(letter) == false) {
+      list[letter] = value;
+    } else {
+      list[letter] = list[letter] + ", " + value;
+    }  
+
+    // модифицираме DOM
+    const index = letter.charCodeAt(0) - 65;
+    items[index].textContent = list[letter];
+
+    input.value = "";
+  }
+}
+*/
+
+function solve(){
+    document.getElementsByTagName('button')[0].onclick = function exe (){
+        let inputName = document.getElementsByTagName('input')[0];
+
+        let firstLetterCode = (inputName.value).toLocaleUpperCase().charCodeAt(0);
+
+        // accessing the catalogue
+        let a = document.getElementsByTagName('li')[firstLetterCode - 65];
+
+        // making the name start with Upper letter and the rest with Lower letters
+        let nameToPush = (inputName.value[0]).toLocaleUpperCase() + (inputName.value).slice(1).toLocaleLowerCase();
+
+        (a.textContent === '') ? a.textContent = nameToPush : a.textContent += ', ' + nameToPush;
+
+        inputName.value = '';
+    }
 }
