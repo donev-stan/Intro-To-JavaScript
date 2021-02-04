5dedb4738136e39e5db0cf2d67ba71eb1d31ee26
donev-stan <77002111+donev-stan@users.noreply.github.com>
2021-02-05 00:30:19 +0200

    learning DOM

--- 3. Advanced/4. DOM/1. Introduction/00. Demo/DOM Traversal/script.js
+++ 3. Advanced/4. DOM/1. Introduction/00. Demo/DOM Traversal/script.js
@@ -1,1 +1,8 @@
- 
+const grandparent = document.querySelector('#grandparentID');
+console.log(grandparent);
+
+// changeColor(grandparent);
+
+// function changeColor(element){
+//     element.style.backgroundColor = "#333";
+// } 
--- 3. Advanced/4. DOM/1. Introduction/03. Growing Word/solution.js
+++ 3. Advanced/4. DOM/1. Introduction/03. Growing Word/solution.js
@@ -1,12 +1,30 @@
+let color;
+let titleSize = 20;
+
+[...document.querySelector('#colors').querySelectorAll('button')].forEach(btn => {
+  btn.addEventListener('click', () => color = btn.innerText);
+});
+
 function growingWord() {
+  const title = document.getElementsByTagName('h1')[0];
 
-  let initialSize = 2;
+  title.style.fontSize = `${titleSize}px`;
+  titleSize += 2;
+  if (titleSize >= 50) {titleSize = 20};
 
-  let title = document.getElementsByTagName("h1")[0];
-
-  title.style.fontSize = initialSize + "px";
-
-
-
-  console.log(initialSize);
+  title.style.color = color;
 }
+
+
+/*
+<div id="blueDiv">Blue</div>
+<div id="greenDiv">Green</div>
+<div id="redDiv">Red</div>
+*/
+// const blue = document.querySelector('#blueDiv');
+// const green = document.querySelector('#greenDiv');
+// const red = document.querySelector('#redDiv');
+
+// blue.addEventListener('click', () => color = blue.innerText);
+// green.addEventListener('click', () => color = green.innerText);
+// red.addEventListener('click', () => color = red.innerText);
