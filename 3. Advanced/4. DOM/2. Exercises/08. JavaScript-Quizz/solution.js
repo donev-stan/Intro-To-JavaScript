e23fbe930cc2736a9ab988f353bd62ec07412443
donev-stan <77002111+donev-stan@users.noreply.github.com>
2021-02-06 14:27:00 +0200

    DOM first chapter done

--- 3. Advanced/4. DOM/2. Exercises/08. JavaScript-Quizz/solution.js
+++ 3. Advanced/4. DOM/2. Exercises/08. JavaScript-Quizz/solution.js
@@ -1,3 +1,37 @@
 function solve() {
-  //TODO...
+  
+  let points = 0;
+  let sectionIndex = 0;
+  const section = document.querySelectorAll('section');
+
+  [...document.querySelectorAll('.answer-text')].forEach(btn => {
+    btn.addEventListener('click', answerAction);
+  });
+
+  function answerAction(e){
+    const answer = e.target.innerText;
+    switch (answer) {
+      case 'onclick':
+      case 'JSON.stringify()':
+      case 'A programming API for HTML and XML documents':
+        points++;
+        break;
+    }
+
+    section[sectionIndex].className = 'hidden';
+    sectionIndex++;
+    if (section[sectionIndex]) { section[sectionIndex].className = 'block'; }
+    else { showResult(); }
+  }
+
+  function showResult(){
+
+    const ul = document.querySelector('#results');
+    const h1 = ul.querySelector('h1');
+
+    ul.style.display = "block";
+
+    if (points < 3) { h1.innerText = `You have ${points} right answers!`; }
+    else { h1.innerText = `You are recognized as top JavaScript fan!`; }
+  }
 }
--- 3. Advanced/4. DOM/2. Exercises/09. Shopping-Cart/solution.js
+++ 3. Advanced/4. DOM/2. Exercises/09. Shopping-Cart/solution.js
@@ -1,3 +1,41 @@
 function solve() {
-   //TODO...
+   
+   const cart = {};
+   const productList = [];
+   let totalPrice = 0;
+
+   document.querySelectorAll('.add-product').forEach(addBtn => {
+      addBtn.addEventListener('click', addToCart);
+   });
+   document.querySelector('.checkout').addEventListener('click', checkout);
+
+   const cartSection = document.querySelector('textarea');
+
+   function addToCart(e){
+      const product = e.target.parentElement.parentElement;
+      const productName = product.querySelector('.product-title').textContent;
+      const productPrice = product.querySelector('.product-line-price').textContent;
+
+      const msg = `Added ${productName} for ${productPrice} to the cart.\n`;
+      cartSection.append(msg);
+
+      if (cart.hasOwnProperty(productName) == false) {
+         cart[productName] = 0;
+      }
+      cart[productName] += Number(productPrice);
+   }
+
+   function checkout(e) {
+      Object.keys(cart).forEach(product => {
+         productList.push(product);
+         totalPrice += Number(cart[product]);
+      });
+      
+      const msg = `You bought ${productList.join(', ')} for ${totalPrice.toFixed(2)}.`;
+      cartSection.append(msg);
+
+      [...document.querySelectorAll('button')].forEach(btn => btn.disabled = true);
+
+      totalPrice = 0;
+   }
 }
