f8cfb81c5068415fd93f966be139b72d4511864b
donev-stan <77002111+donev-stan@users.noreply.github.com>
2021-02-07 14:09:04 +0200

    DOM Manipulation

--- 3. Advanced/5. DOM Manipulations/1. Introduction/07. Dynamic-Validation/app.js
+++ 3. Advanced/5. DOM Manipulations/1. Introduction/07. Dynamic-Validation/app.js
@@ -1,3 +1,11 @@
 function validate() {
-    console.log('TODO:...');
+
+    const regex = /^[a-z-\.]+@[a-z\.]+\.[a-z]{2,4}/;
+    const emailInput = document.getElementById('email');
+    emailInput.addEventListener('change', function () {
+        emailInput.classList.remove('error');
+        const value = emailInput.value;
+        if (value.match(regex)) { return; }
+        emailInput.classList.add('error');
+    });
 }
