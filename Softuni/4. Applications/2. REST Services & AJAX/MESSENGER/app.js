960d55f43890091645d1aebfda82866357d35d53
donev-stan <77002111+donev-stan@users.noreply.github.com>
2021-03-30 01:57:29 +0300

    ajax *

--- Softuni/4. Applications/2. REST Services & AJAX/MESSENGER/app.js
+++ Softuni/4. Applications/2. REST Services & AJAX/MESSENGER/app.js
@@ -1,5 +1,49 @@
 function attachEvents() {
-    console.log('TODO...');
+    const url = `https://rest-messanger.firebaseio.com/messanger.json`;
+
+    const refreshBtn = document.getElementById('refresh');
+    refreshBtn.addEventListener('click', refresh);
+    
+    const submitBtn = document.getElementById('submit');
+    submitBtn.addEventListener('click', submit);
+
+    const textArea = document.getElementById('messages');
+    
+    const authorInput = document.getElementById('author');
+    const contentInput = document.getElementById('content');
+
+    function refresh(e){
+        //cleanup
+        textArea.textContent = '';
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+
+                console.log(data);
+
+                Object.entries(data).forEach(([keyID, {author, content}]) => {
+                    textArea.textContent += `${author}: ${content} \n`;
+                });
+            });
+    }
+
+    function submit(e){
+
+        const message = {
+            author: authorInput.value,
+            content: contentInput.value
+        }
+
+        const messageAsJSON = JSON.stringify(message);
+
+        fetch(url, {method: 'POST', body: messageAsJSON})
+
+        authorInput.value = '';
+        contentInput.value = '';
+
+        refresh();
+    }
 }
 
 attachEvents();
--- Softuni/4. Applications/2. REST Services & AJAX/PHONEBOOK/app.js
+++ Softuni/4. Applications/2. REST Services & AJAX/PHONEBOOK/app.js
@@ -1,5 +1,62 @@
 function attachEvents() {
-    console.log('TODO...');
+    const url = `https://phonebook-nakov.firebaseio.com/phonebook.json`;
+
+    let loadBtn = document.querySelector("#btnLoad");
+    loadBtn.addEventListener("click", load);
+    
+    let createBtn = document.querySelector("#btnCreate");
+    createBtn.addEventListener('click', create);
+
+    let ul = document.querySelector("#phonebook");
+
+    
+    function load(e) {
+
+        //cleanup
+        while (ul.firstChild) ul.removeChild(ul.lastChild);
+        
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data); // { {}, {}, {}... }
+
+                Object.entries(data).forEach(([id, { person, phone }]) => {
+                    const deleteURL = `https://phonebook-nakov.firebaseio.com/phonebook/${id}.json`;
+
+                    let li = document.createElement("li");
+                    li.textContent = `${person}: ${phone}`;
+
+                    let deleteBtn = document.createElement("button");
+                    deleteBtn.textContent = "Delete";
+                    deleteBtn.addEventListener("click", () => {
+                        fetch(deleteURL, {method: 'DELETE'});
+                       
+                    });
+                    // remove li from view
+
+                    li.appendChild(deleteBtn);
+                    ul.appendChild(li);
+                });
+            });
+    }
+
+    function create(e){
+        const personNameInput = document.querySelector('#person');
+        const personPhoneInput = document.querySelector('#phone');
+       
+        const contact = {
+            person: personNameInput.value,
+            phone: personPhoneInput.value
+        };
+
+        const contactAsJSON = JSON.stringify(contact);
+
+        fetch(url, {method: 'POST', body: contactAsJSON});
+    }
+
+    function deletE(e){
+        
+    }
 }
 
 attachEvents();
