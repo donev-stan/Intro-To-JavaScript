28ec5770887b0d1aee7888e7c2571798f62222f8
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2021-06-13 19:57:54 +0300

    handling request pathnames

--- Softuni/5. Back-end/1. Intro to NodeJS/node-js web server/index.js
+++ Softuni/5. Back-end/1. Intro to NodeJS/node-js web server/index.js
@@ -1,24 +1,37 @@
 // Creating web server
 
-const http = require('http');
-const url = require('url');
-const qs = require('querystring');
+const http = require("http");
+const url = require("url");
+const qs = require("querystring");
 
 const port = 5000;
 
 const requestHandler = (request, response) => {
-    console.log(` - request made`);
-    console.log(` - request medthod: ${request.method}`);
-    console.log(` - request url: ${request.url}`);
+  console.log(` - request made`);
+  console.log(` - request medthod: ${request.method}`);
+  console.log(` - request url: ${request.url}`);
 
-    const requestUrlParsed = url.parse(request.url);
-    const queryParams = qs.parse(requestUrlParsed.query);
-    console.log(` - request pathname: ${requestUrlParsed.pathname}`);
-    console.log(queryParams);
+  const requestUrlParsed = url.parse(request.url);
+  const queryParams = qs.parse(requestUrlParsed.query);
+  console.log(` - request pathname: ${requestUrlParsed.pathname}`);
+  console.log(queryParams);
 
+  switch (requestUrlParsed.pathname) {
+    case "/phones":
+      response.write("Welcome to /phones section");
+      break;
 
-    response.write('Hello World!')
-    response.end();
+    case "/pc":
+      response.write("Welcome to /pc section");
+      break;
+    default:
+      response.write("Hello to home page");
+      break;
+  }
+
+  response.end();
 };
 
-http.createServer(requestHandler).listen(port, () => console.log(`Server is listening on port ${port}...`));
+http
+  .createServer(requestHandler)
+  .listen(port, () => console.log(`Server is listening on port ${port}...`));
