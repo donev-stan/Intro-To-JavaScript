37e08f770031501b87f37b5f1cd14a9ce4e6dc3a
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2021-07-26 21:52:54 +0300

    express

--- Softuni/5. Back-end/3. ExpressJS & Templating/catagram/server.js
+++ Softuni/5. Back-end/3. ExpressJS & Templating/catagram/server.js
@@ -3,8 +3,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    console.log(`Hello Server`);
-    response.send(`Hello World from express`);
+    response.send(`Hello world from express`);
+});
+
+app.post('/create-cat', (request, response) => {
+    console.log('Create a cat request');
+
+    res.status(201);
+    response.send(`Cat created!`); // or =>
+    // res.status(201).send(`Cat created!`);
+});
+
+app.all('/', (request, response) => {
+    console.log(`Handle all requests`);
 });
 
 app.listen(5000, () => console.log(` --->>> Server is running on port 5000... <<<--- `));
