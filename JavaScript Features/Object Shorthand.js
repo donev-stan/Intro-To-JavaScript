c6d5ea1d574ad7e528e83db284df8cf581440be3
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2021-07-26 23:37:48 +0300

    object shorthand

---
+++ JavaScript Features/Object Shorthand.js
@@ -0,0 +1,13 @@
+const name = 'Stan';
+const favoriteFood = 'Broccoli';
+
+// const stan = {
+//     name: name,
+//     favoriteFood: favoriteFood
+// };
+
+// instaed: 
+const stan = {
+    name,
+    favoriteFood,
+};
