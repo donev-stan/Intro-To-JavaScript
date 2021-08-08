f525747b0eeae301e3f212fcc3b776a875499aa5
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2021-08-09 02:41:54 +0300

    download response

--- Softuni/5. Back-end/3. ExpressJS & Templating/catagram/server.js
+++ Softuni/5. Back-end/3. ExpressJS & Templating/catagram/server.js
@@ -8,24 +8,25 @@ app.get('/', (request, response) => {
 
 app.post('/create-cat', (request, response) => {
     console.log('Create a cat request');
-    response.status(201);
-    response.send(`Cat created!`); 
-    // or =>
-    // response.status(201).send(`Cat created!`);
+    response.status(201).send(`Cat created!`);
 });
 
-app.get('/cats/:catID', (request, response) => {
+app.get('/cats/:catID', (request, response) => { // '/cats/:catID(\\d+)' istead of the if check below
     console.log(request.params);
 
     if (!/\d+/.test(request.params.catID)) {
         return response.status(404).send('You need to specify cat ID');
     }
-    
+
     response.send(`You are looking for a cat profile: ${request.params.catID}`);
 });
 
 app.all('/', (request, response) => {
     console.log(`Handle all requests`);
+});
+
+app.get('/download', (request, response) => {
+    response.download('./views/home.html');
 });
 
 app.listen(5000, () => console.log(` --->>> Server is running on port 5000... <<<--- `)); 
